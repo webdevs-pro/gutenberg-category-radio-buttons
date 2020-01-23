0e980e4848a6dd0a9f61d0aafe5a572a80c12411
WebDevs.pro <shock_31337@ukr.net>
2020-01-23 03:30:20 +0200

    Add files via upload

---
+++ gutenberg-category-radio-buttons.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  Plugin Name: AI Gutenberg Category Radio Buttons
+  Plugin URI: 
+  Description: This plgin replace default category select checkboxes to radio buttons in Gutenberg editor
+  Author: Alex Ischenko
+  Author URI: https://web-devs.pro
+  Version: 1.0
+ */
+
+
+add_action( 'admin_enqueue_scripts', function(){
+	if ( ! $screen = get_current_screen() ) {
+		return;
+	}
+
+	$post_types = [ 'post', 'page' ];
+	if ( in_array( $screen->id, $post_types ) ) {
+		// Make certain `wp-editor` is added as a dependency.
+		wp_enqueue_script( 'ai-radio-cat-selector', plugin_dir_url( __FILE__ ) . '/js/ai-radio-cat-selector.js', [ 'wp-editor' ] );
+	}
+} );
